4a30de43f553b7ca46a15af3b19bbc2d800bd6c9
Maxwell Flood <maxwell@grotesk.studio>
2020-04-04 08:52:00 +0100

    Changed layout for desktop

--- site/templates/quiz.php
+++ site/templates/quiz.php
@@ -8,18 +8,18 @@
 
           <div class="quiz-wrapper">
 
-            <div class="col-12 ">
-                <div class="typeform-widget-wrapper">
-                  <div class="typeform-widget" data-url="https://burlingtongames.typeform.com/to/BLQeSf" data-transparency="100" data-hide-headers=true data-hide-footer=true style="width: 100%; height: 400px;"></div>
-                  
-                  <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script>
-                </div>
+            <div class="col-6">
+              <div class="twitch-widget-wrapper">
+                <iframe src="https://player.twitch.tv/?channel=burlingtongames" frameborder="0" allowfullscreen="true" scrolling="no"></iframe>
+                <!-- <a href="https://www.twitch.tv/burlingtongames?tt_content=text_link&tt_medium=live_embed" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px; text-decoration:underline;">Watch live video from burlingtongames on www.twitch.tv</a> -->
               </div>
-              
-              <div class="col-12 ">
-                <div class="twitch-widget-wrapper">
-                  <iframe src="https://player.twitch.tv/?channel=burlingtongames" frameborder="0" allowfullscreen="true" scrolling="no"></iframe>
-                  <!-- <a href="https://www.twitch.tv/burlingtongames?tt_content=text_link&tt_medium=live_embed" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px; text-decoration:underline;">Watch live video from burlingtongames on www.twitch.tv</a> -->
+            </div>
+
+            <div class="col-6">
+                <div class="typeform-widget-wrapper">
+                  <div class="typeform-widget" data-url="https://burlingtongames.typeform.com/to/BLQeSf" data-transparency="100" data-hide-headers=true data-hide-footer=true></div>
+                  
+                  <!-- <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script> -->
                 </div>
               </div>
 
